951d39f3677e438932e2591ed4058467c5e1a2aa
Paula <paulabraojosmartinez@gmail.com>
2024-04-30 08:56:28 +0200

    create search bar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,31 +14,35 @@ export class AppComponent {
   searchQuery: string = ''; //Declaración de searchquery
 
   constructor(private readonly userService: UsersService) {
-    // Obtener la lista de usuarios del servicio al inicializar el componente
+   
     this.users = [];
-    this.userService.getUsers().subscribe(response => this.users = response);
+    this.userService.getUsers().subscribe(users => {
+      this.users = users.sort((a, b) => a.name.localeCompare(b.name)); //para ordenar alfabéticamente los contactos
+    });
   }
 
-  // Mostrar los detalles del usuario seleccionado
+ 
   showDetails(user: User) {
     this.selectedUser = user;
   }
 
-  // Ocultar los detalles del usuario
+ 
   hideDetails() {
     this.selectedUser = null;
   }
 
-  // Seleccionar un usuario
+ 
   selectUser(user: User) {
     this.selectedUser = user;
   }
 
-  // searchResults: User[] = [];
+ 
 
   searchContacts() {
     if (!this.searchQuery) {
-      this.userService.getUsers().subscribe(response => this.users = response);
+      this.userService.getUsers().subscribe(users => {
+        this.users = users.sort((a, b) => a.name.localeCompare(b.name));
+      });
     } else {
       this.userService.getUsers().subscribe(users => {
         this.users = users.filter(user =>
@@ -50,4 +54,3 @@ export class AppComponent {
     }
   }
 }
-
